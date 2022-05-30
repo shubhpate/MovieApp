99ffdf159287bbbd23d5ef86480f7104f56a8805
shubhpate <99386303+shubhpate@users.noreply.github.com>
2022-05-30 09:50:58 +0530

    Add
    Movie App Data

---
+++ MovieApp.Data/Repositories/IMovie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MovieApp.Entity;
+
+namespace MovieApp.Data.Repositories
+{
+    public interface IMovie
+    {
+        string Register(MovieModel userModel);
+        string Delete(int id);
+        object GetMovies();
+        string Update(MovieModel u);
+    }
+}
